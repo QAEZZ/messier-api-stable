407d6610dcae0b5e8f1ae6476d35a10896588bbb
QÆZZ <65181690+QAEZZ@users.noreply.github.com>
2022-09-09 10:57:12 -0500

    Update java_util.py

--- handlers/java_util.py
+++ handlers/java_util.py
@@ -6,31 +6,35 @@ class java_util(web.Application):
     def __init__(self, app):
         self.app = app
 
+    # /convertHashmapJson
     async def convertHashmapJson(self, request):
-        """
-        to convert json to hashmap:
-         - /convertHashapJson?json={"your":"json"}
-
-        to convert hashmap to json
-         - /convertHashapJson?hashmap={your=hashmap}
-        """
         if "hashmap" in request.query:
             rs_json = {}
-            hashmap = request.query["hashmap"].replace("{", "").replace("}", "").replace(", ", ",")
+            hashmap = request.query["hashmap"].replace("{", "").replace(
+                "}", "").replace(", ", ",")
             for pair in hashmap.split(","):
                 key, value = pair.split("=")
                 rs_json[key] = value
+
             return web.json_response(rs_json)
+
         elif "json" in request.query:
             rs_json = json.loads(request.query["json"])
             hashmap = "{"
             for key in rs_json:
-                hashmap += f"{key}={rs_json[key]},"
-            hashmap = hashmap[:-1] + "}"
+                hashmap += f"{key}={rs_json[key]}"
+            print(rs_json)
+            if hashmap[-1] == ",":
+              hashmap = hashmap[:-1]
+            hashmap = hashmap + "}"
             return web.Response(text=hashmap)
+# shit school started, ill be in 40 min
         else:
-            return web.Response(text=self.convertHashmapJson.__doc__)
+            return web.FileResponse("./static/html/docs/index.html")
+
+    async def something(self, request):
+      return {"name": "test"}
 
 
 def setup(app):
-    return java_util(app), {"convertHashmapJson": "/convertHashmapJson"}
+    return java_util(app), {"convertHashmapJson": "/convertHashmapJson", "something": "/something"}
