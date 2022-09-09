ef3e9416b817586eb4a645af68919682756cb33c
QÆZZ <65181690+QAEZZ@users.noreply.github.com>
2022-09-09 10:57:41 -0500

    Update messier.py

--- handlers/messier.py
+++ handlers/messier.py
@@ -1,5 +1,3 @@
-# ill create a seperate file for the JSON dump k
-
 from aiohttp import web
 import messiers
 import json
@@ -9,41 +7,72 @@ import random
 
 logging.basicConfig(filename='log.log', level=logging.DEBUG)
 
+
 class _messier(web.Application):
-    def __init__(self, app):
-        self.app = app
+	def __init__(self, app):
+		self.app = app
 
-    # /messier
-    async def messier(self, response):
+# /messier
 
-        if "object" in response.query:
-            print("object in query")
-            # send DSO by messier number
-            messier = messiers.get(object=response.query["object"])
-            print(f"object pre-get is {messier}")
-            return web.json_response(messier)
-        elif "random" in response.query and response.query["random"].lower(
-        ) == "true":
-            # send a random object
-            messier = messiers.get()
-            return web.json_response(messier)
+	async def messier(self, response):
+		if "object" in response.query:
+			print(response.query["object"])
+			if "format" in response.query:
+				print(response.query["format"])
+				if response.query[
+				    "format"] == "xml" or "html" or "yaml" or "visual":
+					messier = messiers.get(object=response.query["object"],
+					                       format=response.query["format"])
 
-        else:
-            return web.FileResponse('./static/html/docs/index.html')
+					if response.query["format"] == "visual":
+						return web.Response(text=messier, content_type="html")
+					else:
+						return web.Response(text=messier)
+			else:
+				messier = messiers.get(object=response.query["object"])
+				return web.json_response(messier)
 
-    # /random
-    async def random(self, response):
-        """
+		else:
+			return web.FileResponse('./static/html/docs/index.html')
+
+# /random
+
+	async def random(self, response):
+		"""
         get random object ☄️
           - /messier/random
         """
-        messier = messiers.get(random.randint(1,110))
-        return web.json_response(messier)
+		if "format" in response.query:
+			print(response.query["format"])
+			if response.query[
+			    "format"] == "xml" or "html" or "yaml" or "visual":
+				messier = messiers.get(object=random.randint(1, 110),
+				                       format=response.query["format"])
 
+				if response.query["format"] == "visual":
+					return web.Response(text=messier, content_type="html")
+				else:
+					return web.Response(text=messier)
+		else:
+			messier = messiers.get(object=response.query["object"])
+			return web.json_response(messier)
+
+
+# /all
+
+	async def all(self, response):
+		"""
+      get all objects
+        - /messier/all
+      """
+		with open("./data/messier.json", "r") as f:
+			data = json.load(f)
+			return web.json_response(data)
 
 
 def setup(app):
-    return _messier(app), {
-        "messier": "/messier",
-        "random": "/messier/random"
-    }
+	return _messier(app), {
+	    "messier": "/messier",
+	    "random": "/messier/random",
+	    "all": "/messier/all"
+	}
