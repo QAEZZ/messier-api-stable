60550dbe702bbef555be177cf02fcd5fbae8b709
QÆZZ <65181690+QAEZZ@users.noreply.github.com>
2022-09-09 10:47:58 -0500

    Update middlewares.py

--- middlewares.py
+++ middlewares.py
@@ -1,47 +1,42 @@
 import aiohttp_jinja2
-import jinja2 # I think I imported too much lmao
+import jinja2
 from aiohttp_jinja2 import *
 from aiohttp import web
 
 
 async def handle_404(request):
-    return aiohttp_jinja2.render_template('./static/html/errors/404.html', request, {})
+    return aiohttp_jinja2.render_template("./static/html/errors/404.html",
+                                          request, {})
 
 
 async def handle_500(request):
-    return aiohttp_jinja2.render_template('./static/html/errors/500.html', request, {})
+    return aiohttp_jinja2.render_template("./static/html/errors/500.html",
+                                          request, {})
 
 
-def create_error_middleware(overrides):
+# shoutout to stack overflow
 
-    @web.middleware
-    async def error_middleware(request, handler):
 
-        try:
-            response = await handler(request)
-
-            override = overrides.get(response.status)
-            if override:
-                return await override(request)
-
-            return response
-
-        except web.HTTPException as ex:
-            override = overrides.get(ex.status)
-            if override:
-                return await override(request)
-
-            raise
-
-    return error_middleware
+@web.middleware
+async def error_middleware(request, handler):
+    try:
+        response = await handler(request)
+        # this is needed to handle ``return web.HTTPNotFound()`` case
+        if response.status == 404:
+            return web.FileResponse("./static/html/errors/404.html")
+        return response
+    except web.HTTPException as ex:
+        # this is needed to handle ``raise web.HTTPNotFound()`` case
+        if ex.status == 404:
+            return web.FileResponse("./static/html/errors/404.html")
+        raise
+    # this is needed to handle non-HTTPException
+    except Exception:
+        return web.FileResponse("./static/html/errors/500.html")
 
 
 def setup_middlewares(app):
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('.'))
-    app.router.add_static('/static', "static")
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("."))
+    app.router.add_static("/static", "static")
 
-    error_middleware = create_error_middleware({
-        404: handle_404,
-        500: handle_500
-    })
     app.middlewares.append(error_middleware)
